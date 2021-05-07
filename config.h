6881c50f02a6d0e8eae8f3ceeedb01c70deca282
Brian Ryall <brianryall@fastmail.com>
2021-05-07 16:04:40 -0400

    feat: apply dmenu-grid patch

--- config.h
+++ config.h
@@ -18,6 +18,7 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
